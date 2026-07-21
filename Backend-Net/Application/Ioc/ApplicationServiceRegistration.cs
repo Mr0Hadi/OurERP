153dac561db2083ae26445475c6bfa8525830fdc
alisinai <alisinai850@gmail.com>
2026-07-21 19:18:33 +0330

    .

---
+++ Backend-Net/Application/Ioc/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Ioc
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(OldSendSmsCommand).Assembly);
+            });
+
+            services.AddValidatorsFromAssembly(typeof(OldSendSmsCommandValidator).Assembly);
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
+
+            return services;
+        }
+    }
+}
